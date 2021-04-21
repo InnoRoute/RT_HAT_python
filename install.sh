4cc6534b82e9bf4876ce3afed2acffc638f79729
Ulbricht, Marian <ulbricht@innoroute.de>
2021-04-21 21:39:38 +0200

    update

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 IP_addr="192.168.1.110"
 python3 -m build &
+ssh pi@$IP_addr 'sudo python3 -m pip uninstall rt-hat-inr -y '
 ssh pi@$IP_addr 'python3 -m pip uninstall rt-hat-inr -y '&
 wait
+ssh pi@$IP_addr 'rm rt_hat_inr-*-py3-none-any.whl'
 scp dist/rt_hat_inr-*-py3-none-any.whl pi@$IP_addr:~/ &
 scp example/* pi@$IP_addr:~/ &
 wait
 ssh pi@$IP_addr 'python3 -m pip install rt_hat_inr-*-py3-none-any.whl'
-#ssh pi@$IP_addr 'python3 TAS_test.py'
+ssh pi@$IP_addr 'python3 TAS_test.py'
 
