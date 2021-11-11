714e1ce6b318d761ed4017df8dc40dd7aaef28ca
Ulbricht, Marian <ulbricht@innoroute.de>
2021-11-11 18:09:48 +0100

    doku

--- example/TAS_test.py
+++ example/TAS_test.py
@@ -5,8 +5,8 @@ RT_HAT_TAS.DEBUG_OUTPUT=True
 RT_HAT_TAS.init("/usr/share/InnoRoute/hat_env.sh")
 port=2
 my_GCL=[
-	[0x00,48000000], #all gates open for 48ms
-	[0x01,48000000] #gate 0 closed for 48ms
+	[0x00,48000000], #all gates closed for 48ms
+	[0x01,48000000] #gate 0 open for 48ms
 	] 
 RT_HAT_TAS.set_GCL(my_GCL,port) #set GCL of port
 print(RT_HAT_TAS.get_GCL(port)) #print GCL
