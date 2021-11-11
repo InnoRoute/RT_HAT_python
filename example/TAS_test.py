986bebc1d5c14cedfde3329a2183e4114b5327a4
Ulbricht, Marian <ulbricht@innoroute.de>
2021-11-11 17:44:35 +0100

    doku

--- example/TAS_test.py
+++ example/TAS_test.py
@@ -6,15 +6,10 @@ RT_HAT_TAS.init("/usr/share/InnoRoute/hat_env.sh")
 port=2
 my_GCL=[
 	[0x00,48000000], #all gates open for 48ms
-	[0x01,48000000] #all gates closed for 48ms
+	[0x01,48000000] #gate 0 closed for 48ms
 	] 
 RT_HAT_TAS.set_GCL(my_GCL,port) #set GCL of port
 print(RT_HAT_TAS.get_GCL(port)) #print GCL
 RT_HAT_TAS.apply(port)#apply tas settings to port 
 
-
-#TrustNode config
-#TNtsnctl Change_entry -i0 -S0xff -I480000 -a2 -P0     
-#TNtsnctl Change_entry -i1 -S0x00 -I480000 -a2 -P0  
-#TNtsnctl apply -C0 -b0 -d0 -P0 
 
