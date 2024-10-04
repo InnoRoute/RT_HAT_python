e05fa54803fccbed7d673d3e9a1b6f262ffa3e42
Marian Ulbricht <ulbricht@innoroute.de>
2024-10-04 22:00:42 +0200

    addforcelinkspeed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="rt-hat-inr", # Replace with your own username
-    version="0.6.1",
+    version="0.6.2",
 
     author="Marian Ulbricht",
     author_email="ulbricht@innoroute.de",
@@ -19,7 +19,7 @@ setuptools.setup(
    'transitions',
    'asyncio'
 		],
-    scripts=['bin/INR_change_bitstream','bin/INR_FPGA_status','bin/INR_FPGA_license','bin/INR-config','bin/INR_PHYd','bin/INR_PHYd_uc0'],
+    scripts=['bin/INR_change_bitstream','bin/INR_FPGA_status','bin/INR_FPGA_license','bin/INR-config','bin/INR_PHYd','bin/INR_PHYd_uc0','bin/INR_PHYd_force_both_single'],
     long_description_content_type="text/markdown",
     url="https://github.com/InnoRoute/RealtimeHAT",
     project_urls={
